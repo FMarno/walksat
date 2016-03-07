fb8b73036ea1b93a62b3af84eb69a6062db8f131
Finlay <fsm@st-andrews.ac.uk>
2016-03-07 22:34:03 +0000

    I dont know anymore

--- src/parser.c
+++ src/parser.c
@@ -14,47 +14,43 @@ void clearBuffer(char buffer[BUFFER_SIZE]){
 }
 
 
-int** parseData(FILE* fp, int* vars, int* clauses){
+void parseHeader(FILE* fp, int* vars, int* clauses){
  char* line = (char*)malloc(sizeof(char)*BUFFER_SIZE);
  size_t len = 0;
-  while(1){
-    getline(&line, &len, fp);
-    if (line[0] != 'c'){ break; }
-  }
+ do {
+ 	getline(&line, &len, fp);
+ } while(line[0] == 'c');
 
   sscanf(line, "p cnf %d  %d", vars, clauses);
-  int** clausesContainingLiteral = (int**) calloc((*vars)*2, sizeof(int*));
-  for (int i =0; i< (*vars)*2; i++){
-  	clausesContainingLiteral[i] = calloc(*clauses, sizeof(int));
-  }
-
-  int i = 0;
-  int token = 0;
-  while (i < *clauses){
-    clearBuffer(line);
-    nextToken(line, fp);
-
-    if (line[0] == '0'){
-    	//end of line
-      i++;
-    } else if (line[0] == '-'){
-    	//negated atoms
-      line++;
-      sscanf(line, "%d", &token);
-      line--;
-      token--;
-      token = token*2 + 1;
-      clausesContainingLiteral[token][i] = 1;
-    } else {
-    	//positive atoms
-      sscanf(line, "%d", &token);
-      token--;
-      token *= 2;
-      clausesContainingLiteral[token][i] = 1;
-    }
-  }
   free(line);
+}
 
-  return clausesContainingLiteral;
+void parseData(FILE* fp, int vars, int clauses, int clausesContainingLiteral[vars*2][clauses]){
+	char* line = (char*)malloc(sizeof(char)*BUFFER_SIZE);
+	int i = 0;
+    int token = 0;
+	while (i < clauses){
+	  clearBuffer(line);
+	  nextToken(line, fp);
 
+	  if (line[0] == '0'){
+	  	//end of line
+	    i++;
+	  } else if (line[0] == '-'){
+	  	//negated atoms
+	    line++;
+	    sscanf(line, "%d", &token);
+	    line--;
+	    token--;
+	    token = token*2 + 1;
+	    clausesContainingLiteral[token][i] = 1;
+	  } else {
+	  	//positive atoms
+	    sscanf(line, "%d", &token);
+	    token--;
+	    token *= 2;
+	    clausesContainingLiteral[token][i] = 1;
+	  }
+	}
+    free(line);
 }
