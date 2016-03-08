a79e3677547b5a1e8411e186eed8a2217c99654d
Finlay <fsm@st-andrews.ac.uk>
2016-03-08 18:49:14 +0000

    added command line inputs

--- src/main.c
+++ src/main.c
@@ -1,14 +1,28 @@
 #include "walksat.h"
 
-int main(){
-	//FILE* fp = fopen("sat.cnf","r"); 
-  FILE* fp = fopen("UF150.645.100/uf150-01.cnf", "r");
-  if (fp == NULL){
-    printf("file not found\n");
-    exit(1);
-  }
-  	srand(time(NULL));
-  	srand48(time(NULL));
-  	
-  walksat(fp, 10000000, 0.9);
+int main(int argc, char** argv){
+	if (argc != 4){
+		printf("Wrong number of arguments\n");
+		printf("useage: ./WalkSAT [file name] [threshold] [probability]\n");
+	} else {
+		//FILE* fp = fopen("sat.cnf","r"); 
+		FILE* fp = fopen(argv[1], "r");
+		if (fp == NULL){
+			printf("file not found\n");
+			exit(1);
+		}
+		srand(time(NULL));
+		srand48(time(NULL));
+
+		int threshold; float probability;
+		sscanf(argv[2], "%d", &threshold);
+		
+		sscanf(argv[3], "%f", &probability);
+		if (probability > 0 && probability < 1){
+			walksat(fp, threshold, probability);
+		} else {
+			printf("Probability in inappriopriate range.\n\
+				\rPlease select a number between (0,1) exclusive\n");
+		}
+	}
 }
